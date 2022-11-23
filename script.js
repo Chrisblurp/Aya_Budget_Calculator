0d40758c32deb8e8817cf96bdea63c599f291198
Chrisblurp <adeyemi504@gmail.com>
2022-11-24 00:21:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let income = document.getElementById("income").value;
+// let age = document.getElementById("age").value;
+
+const calculateBudget = (event) => {
+let income = document.forms['calculate-budgetform']['income'].value;
+let age = document.forms['calculate-budgetform']['age'].value
+
+if(income ){
+
+}
+}
