f53da82ec6ad0c1fcf95193b53c922d4a62d4c3b
Chrisblurp <adeyemi504@gmail.com>
2022-11-26 00:23:20 +0100

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 // let income = document.getElementById("income").value;
 // let age = document.getElementById("age").value;
 
-const calculateBudget = (event) => {
-let income = document.forms['calculate-budgetform']['income'].value;
-let age = document.forms['calculate-budgetform']['age'].value
+const calculateBudget = (e) => {
+   
+    
+    // first get the value of the income and age of the user
+// let income = document.forms['calculate-budgetform']['income'].value;
+let age = document.forms['calculate-budgetform']['age'].value;
 
-if(income ){
+if(age > 16 ) {
+    document.loginForm.action = "budget.html";
 
+    alert("you are too young to have an income");
+
+}
+else{
+    
+    return false;
 }
 }
